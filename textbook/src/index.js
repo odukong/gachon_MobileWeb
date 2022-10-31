904036469bc590c6303505c51979283b930c56e7
odukong <odukon@gmail.com>
2022-10-31 16:16:06 +0900

    실습일지10-1

--- textbook/src/index.js
+++ textbook/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './client/App';
 import './index.css';
+import PageColor from './chapter_14/PageColor';
 
   const root=ReactDOM.createRoot(document.getElementById("root"));
   root.render(
-      <div><App/></div>
+      <div><PageColor/></div>
   );
