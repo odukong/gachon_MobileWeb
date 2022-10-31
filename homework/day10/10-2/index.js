8e69bdb2509dc1be435f66c625cdfc49eab110f2
odukong <91336314+odukong@users.noreply.github.com>
2022-10-31 17:23:31 +0900

    10-2

--- homework/day10/10-2/index.js
+++ homework/day10/10-2/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import PageColor from './chapter_14/PageColor';
+import ContextHook from './ContextHook';
 
   const root=ReactDOM.createRoot(document.getElementById("root"));
   root.render(
-      <div><PageColor/></div>
+      <div><ContextHook/></div>
   );
