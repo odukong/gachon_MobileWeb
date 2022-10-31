75ad6d08b3fc9a81609dbc7f4006d0008b462608
odukong <91336314+odukong@users.noreply.github.com>
2022-10-31 16:42:59 +0900

    Delete textbook directory

--- textbook/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import PageColor from './chapter_14/PageColor';
-
-  const root=ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-      <div><PageColor/></div>
-  );
