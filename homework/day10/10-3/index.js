ee946817084c237f1e9a76df0d9f04b7eda36b62
odukong <91336314+odukong@users.noreply.github.com>
2022-11-01 10:16:08 +0900

    10-3

--- homework/day10/10-3/index.js
+++ homework/day10/10-3/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import PageColor from './chapter_14/PageColor';
+import PageColor from './PageColor';
 
   const root=ReactDOM.createRoot(document.getElementById("root"));
   root.render(
